d43b53c85c3eb653ae2ca3263c03f88aa6fe9ba3
Yogihafidh <hafidhyogi@gmail.com>
2025-04-08 22:00:42 +0700

    feat: add header section

--- src/sections/Header.tsx
+++ src/sections/Header.tsx
@@ -1,3 +1,23 @@
 export const Header = () => {
-  return <div>Header Section</div>;
+  return (
+    <div className="flex justify-center items-center relative top-3  ">
+      <nav className="flex p-1 rounded-full shadow-md border border-gray-100 gap-1 ">
+        <a href="#" className="nav-item">
+          Home
+        </a>
+        <a href="#" className="nav-item">
+          Project
+        </a>
+        <a href="#" className="nav-item">
+          About
+        </a>
+        <a
+          href="#"
+          className="nav-item bg-gray-900 hover:bg-gray-700 text-white"
+        >
+          Contact
+        </a>
+      </nav>
+    </div>
+  );
 };
